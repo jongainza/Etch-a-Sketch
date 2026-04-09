06cad3ccfbe161ce222bd68621b25d3f0f7b989b
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-04-09 09:14:17 -0700

    add hover effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 console.log('hello world');
-let gridsNumber=16
+let gridsNumber=25
+
 let space = document.querySelector('.div')
 
 let gridsFlex = (100/gridsNumber).toFixed(4)
@@ -9,13 +10,27 @@ let gridsHeigth = gridsFlex
 function createGrid(gridsNumber){
  for (let i=1 ;  i<= gridsNumber * gridsNumber ; i++){
   let grid = document.createElement('div')
+ 
+  grid.textContent=`${i}`
   grid.classList.add('grid')
   grid.style.flex= `1 0 ${gridsFlex}%`
   grid.style.height=`${gridsHeigth
   }%`
+  grid.addEventListener("mouseenter", () => {
+    console.log("hover");
+
+    grid.classList.add("hover");
+  });
+  grid.addEventListener("mouseleave", () => {
+    grid.classList.remove("hover");
+  });
   space.appendChild(grid)
+     
+
+
 
  }
 }
+
 
 createGrid(gridsNumber)
